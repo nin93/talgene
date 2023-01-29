aa4ad5587dd0f4f8344a06937dd60a4a60df2d90
Elia Franzella <eliafranzella@live.it>
2023-01-29 17:36:00 +0100

    fix(system): Make `#current_iteration` actually return the current count

--- src/models/system.cr
+++ src/models/system.cr
@@ -4,11 +4,14 @@ module Talgene
   class System(T)
     include Iterable(T)
 
-    getter current_iteration : Int32 = 0
     getter iterator : Talgene::Generation::GenerationIterator(T)
 
     def initialize(@generation : T, *, @max_iterations : Int32)
       @iterator = Talgene::Generation::GenerationIterator.new @generation, @max_iterations
+    end
+
+    def current_iteration
+      @iterator.elapsed
     end
 
     def each
