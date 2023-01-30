51323bb0f684a049c615676313dde25bcbfaec50
Elia Franzella <eliafranzella@live.it>
2023-01-30 01:17:13 +0100

    feat(genome): Include `Indexable` module

--- src/models/genome.cr
+++ src/models/genome.cr
@@ -3,10 +3,20 @@ require "../modules/fittable"
 module Talgene
   abstract class Genome(T)
     include Talgene::Fittable
+    include Indexable(T)
 
     getter genes : Array(T)
 
     def initialize(@genes : Array(T))
+    end
+
+    @[AlwaysInline]
+    def unsafe_fetch(index : Int)
+      @genes.unsafe_fetch index
+    end
+
+    def size
+      @genes.size
     end
 
     def to_s(io : IO)
